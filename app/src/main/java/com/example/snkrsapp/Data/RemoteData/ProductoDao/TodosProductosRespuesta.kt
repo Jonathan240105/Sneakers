d800da26af1b0fc1aafd596a1f76f45311cb9d53
Jonathan240105 <jonathan.quito@live.u-tad.com>
2026-04-21 15:04:03 +0200

    Commit añadido modelo de clases para la respuesta de la api

---
+++ app/src/main/java/com/example/snkrsapp/Data/RemoteData/ProductoDao/TodosProductosRespuesta.kt
@@ -0,0 +1,11 @@
+package com.example.snkrsapp.Data.RemoteData.ProductoDao
+
+data class TodosProductosRespuesta(
+    val idProducto: Int? = null,
+    val idMarca: Int = 0,
+    val modelo: String = "",
+    val precio: Int = 0,
+    val talla: Int = 0,
+    val uidVendedor: String = "",
+    val imagenUrl: String = ""
+)
