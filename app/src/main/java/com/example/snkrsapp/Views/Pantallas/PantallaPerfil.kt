7367bc1e5b284e9d03b3d90f12f455447912b6cd
Jonathan240105 <jonathan.quito@live.u-tad.com>
2026-05-04 12:20:25 +0200

    Commit cambios en la pantalla de perfil

--- app/src/main/java/com/example/snkrsapp/Views/Pantallas/PantallaPerfil.kt
+++ app/src/main/java/com/example/snkrsapp/Views/Pantallas/PantallaPerfil.kt
@@ -2,15 +2,31 @@ package com.example.snkrsapp.Views.Pantallas
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.material3.*
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -19,15 +35,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.snkrsapp.Data.RemoteData.AutorizacionDao.Usuario
 import com.example.snkrsapp.Views.ViewModels.PerfilViewModel
 
 @Composable
-fun PantallaPerfil(onCerrarSesion: () -> Unit, myViewModel: PerfilViewModel) {
+fun PantallaPerfil(cambiarAConfig: () -> Unit, myViewModel: PerfilViewModel) {
 
     LaunchedEffect(Unit) {
         myViewModel.cargarPerfil()
@@ -36,156 +51,165 @@ fun PantallaPerfil(onCerrarSesion: () -> Unit, myViewModel: PerfilViewModel) {
     val model by myViewModel.model.collectAsState()
     val estadoScroll = rememberScrollState()
 
+    val coleccionEjemplo = listOf("Jordan 1", "Yeezy 350", "Dunk Low", "Air Max 90", "Forum Low")
+    val ventasEjemplo = listOf("New Balance 550", "Travis Scott AJ1")
+    val favoritos = listOf("Jordan 1", "Yeezy 350", "Dunk Low", "Air Max 90", "Forum Low")
+
     Column(
-        modifier = Modifier
+        Modifier
             .fillMaxSize()
             .background(Color(0xFF121212))
             .padding(horizontal = 20.dp)
             .verticalScroll(estadoScroll)
     ) {
-        Spacer(modifier = Modifier.height(40.dp))
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Box(
-                modifier = Modifier
-                    .size(100.dp)
-                    .clip(CircleShape)
-                    .background(Color(0xFF1E1E1E)),
-                contentAlignment = Alignment.Center
-            ) {
-                Icon(
-                    Icons.Default.Person,
-                    contentDescription = null,
-                    Modifier.size(60.dp),
-                    tint = Color.White
-                )
-            }
-            Spacer(Modifier.height(15.dp))
-            Text(
-                "@${model.usuarioActual.nombreUsuario}",
-                color = Color.White,
-                fontSize = 24.sp,
-                fontWeight = Bold
-            )
-            Spacer(Modifier.height(5.dp))
-            Text(
-                model.usuarioActual.email,
-                color = Color.Gray,
-                fontSize = 14.sp
-            )
-        }
-        Spacer(modifier = Modifier.height(40.dp))
-        Text(
-            "Información Personal",
-            Modifier.padding(bottom = 12.dp),
-            color = Color.White,
-            fontSize = 18.sp,
-            fontWeight = Bold
-        )
+        Spacer(Modifier.height(30.dp))
+
         Card(
-            Modifier.fillMaxWidth(),
+            Modifier
+                .fillMaxWidth()
+                .testTag("HeaderPerfil"),
             colors = CardDefaults.cardColors(containerColor = Color(0xFF1E1E1E)),
             shape = RoundedCornerShape(25.dp)
         ) {
-            Column(Modifier.padding(20.dp)) {
-                ItemInfoPerfil(
-                    Icons.Default.Person,
-                    "Nombre Completo",
-                    "${model.usuarioActual.nombreUsuario} ${model.usuarioActual.apellidos}"
-                )
+            Column(
+                Modifier.padding(20.dp), horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(
+                        Modifier
+                            .size(70.dp)
+                            .clip(RoundedCornerShape(18.dp))
+                            .background(Color(0xFF252525)), contentAlignment = Alignment.Center
+                    ) {
+                        Icon(Icons.Default.Person, null, Modifier.size(40.dp), tint = Color.White)
+                    }
+                    Spacer(Modifier.width(15.dp))
+                    Column {
+                        Text(
+                            "@${model.usuarioActual.nombreUsuario}",
+                            color = Color.White,
+                            fontSize = 20.sp,
+                            fontWeight = Bold
+                        )
+                        Text(model.usuarioActual.email, color = Color.Gray, fontSize = 13.sp)
+                    }
+                    Spacer(Modifier.weight(1f))
+                    Column {
+                        Icon(
+                            Icons.Default.Settings,
+                            "",
+                            tint = Color.White,
+                            modifier = Modifier.clickable(
+                                onClick = { cambiarAConfig() }))
+                    }
+                }
                 HorizontalDivider(
-                    Modifier.padding(vertical = 12.dp),
-                    thickness = 1.dp,
-                    color = Color(0xFF252525)
+                    Modifier.padding(vertical = 20.dp), thickness = 1.dp, color = Color(0xFF252525)
                 )
-                ItemInfoPerfil(
-                    Icons.Default.DateRange,
-                    "Fecha de Nacimiento",
-                    model.usuarioActual.fechaNacimiento
-                )
-                HorizontalDivider(
-                    Modifier.padding(vertical = 12.dp),
-                    thickness = 1.dp,
-                    color = Color(0xFF252525)
-                )
-                ItemInfoPerfil(
-                    Icons.Default.Menu,
-                    "User ID",
-                    model.usuarioActual.UID
-                )
+                Row(
+                    Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    EstadisticaItem("Colección", coleccionEjemplo.size.toString())
+                    Box(
+                        Modifier
+                            .width(1.dp)
+                            .height(30.dp)
+                            .background(Color(0xFF333333))
+                    )
+                    EstadisticaItem("En Venta", ventasEjemplo.size.toString())
+                    Box(
+                        Modifier
+                            .width(1.dp)
+                            .height(30.dp)
+                            .background(Color(0xFF333333))
+                    )
+                    EstadisticaItem("Favoritos", favoritos.size.toString())
+                }
             }
         }
         Spacer(Modifier.height(30.dp))
-        Text(
-            "Cuenta",
-            Modifier.padding(bottom = 12.dp),
-            color = Color.White,
-            fontSize = 18.sp,
-            fontWeight = Bold
-        )
-        CardAccion(Icons.Default.Favorite, "Mis Favoritos") {}
-        Spacer(Modifier.height(8.dp))
-        CardAccion(Icons.Default.ShoppingCart, "Mis Pedidos") {}
-        Spacer(Modifier.height(8.dp))
-        CardAccion(Icons.Default.Settings, "Ajustes de Cuenta") {}
-        Spacer(Modifier.height(40.dp))
-        Button(
-            onCerrarSesion,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(55.dp),
-            colors = ButtonDefaults.buttonColors(containerColor = Color.White),
-            shape = RoundedCornerShape(25.dp)
-        ) {
-            Text("Cerrar Sesión", color = Color.Black, fontWeight = Bold, fontSize = 16.sp)
-        }
-        Spacer(Modifier.height(20.dp))
+        listaProductos(
+            titulo = "Mi Colección", items = coleccionEjemplo, VerTodo = {})
+        Spacer(Modifier.height(24.dp))
+        listaProductos(
+            titulo = "Mis Ventas", items = ventasEjemplo, VerTodo = {})
+        Spacer(modifier = Modifier.height(24.dp))
+        listaProductos(
+            "Favoritos", favoritos, {})
     }
 }
 
 @Composable
-fun ItemInfoPerfil(icono: ImageVector, titulo: String, valor: String) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Icon(
-            icono,
-            contentDescription = null,
-            tint = Color.LightGray,
-            modifier = Modifier.size(20.dp)
-        )
-        Spacer(Modifier.width(15.dp))
-        Column {
-            Text(titulo, color = Color.Gray, fontSize = 12.sp)
-            Text(valor, color = Color.White, fontSize = 15.sp, fontWeight = Bold)
-        }
-    }
-}
-
-@Composable
-fun CardAccion(icono: ImageVector, titulo: String, onClick: () -> Unit) {
-    Card(
-        Modifier
-            .fillMaxWidth()
-            .clickable { onClick() },
-        colors = CardDefaults.cardColors(containerColor = Color(0xFF1E1E1E)),
-        shape = RoundedCornerShape(15.dp)
-    ) {
+fun listaProductos(
+    titulo: String, items: List<String>, VerTodo: () -> Unit
+) {
+    Column {
         Row(
-            Modifier.padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
+            Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Icon(
-                    icono,
-                    contentDescription = null,
-                    tint = Color.White,
-                    modifier = Modifier.size(25.dp)
-                )
-                Spacer(Modifier.width(15.dp))
-                Text(titulo, color = Color.White, fontSize = 15.sp)
+            Text(titulo, color = Color.White, fontSize = 18.sp, fontWeight = Bold)
+            Text(
+                "Ver todo",
+                color = Color.Gray,
+                fontSize = 14.sp,
+                modifier = Modifier.clickable { VerTodo() })
+        }
+        Spacer(Modifier.height(16.dp))
+
+        if (items.isEmpty()) {
+            Text(
+                "No hay artículos todavía",
+                color = Color.DarkGray,
+                fontSize = 14.sp,
+                modifier = Modifier.padding(vertical = 20.dp)
+            )
+        } else {
+            LazyRow(
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                items(items) { item ->
+                    Card(
+                        Modifier.size(width = 140.dp, height = 100.dp),
+                        colors = CardDefaults.cardColors(containerColor = Color(0xFF1E1E1E)),
+                        shape = RoundedCornerShape(16.dp)
+                    ) {
+                        Box(
+                            contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()
+                        ) {
+                            Icon(
+                                Icons.Default.ShoppingCart,
+                                contentDescription = null,
+                                tint = Color(0xFF333333),
+                                modifier = Modifier.size(40.dp)
+                            )
+                            Text(
+                                item,
+                                color = Color.White,
+                                fontSize = 12.sp,
+                                modifier = Modifier
+                                    .align(Alignment.BottomCenter)
+                                    .padding(bottom = 8.dp)
+                            )
+                        }
+                    }
+                }
             }
         }
+    }
+}
+
+@Composable
+fun EstadisticaItem(titulo: String, valor: String) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            valor, color = Color.White, fontSize = 18.sp, fontWeight = Bold
+        )
+        Text(
+            titulo, color = Color.Gray, fontSize = 12.sp
+        )
     }
 }
