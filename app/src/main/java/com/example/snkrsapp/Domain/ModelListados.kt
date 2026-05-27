94e4095e8fa7c3fddd307e4ca183c9522286c196
Jonathan240105 <jonathan.quito@live.u-tad.com>
2026-05-27 18:16:19 +0200

    Commit añadidas data class de domain para la pantalla

---
+++ app/src/main/java/com/example/snkrsapp/Domain/ModelListados.kt
@@ -0,0 +1,16 @@
+package com.example.snkrsapp.Domain
+
+data class ModelListados(
+    val cargandoColeccion: Boolean = false,
+    val exitoColeccion: Boolean = false,
+
+    val cargandoVentas: Boolean = false,
+    val exitoVentas: Boolean = false,
+
+    val cargandoCarrito: Boolean = false,
+    val exitoCarrito: Boolean = false,
+
+    val listaColeccion: List<ProductoColeccionItem> = emptyList(),
+    val listaVentas: List<PublicacionPerfilItem> = emptyList(),
+    val listaCarrito: List<PublicacionPerfilItem> = emptyList()
+)
