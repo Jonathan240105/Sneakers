84ce5a4e6c08cc9f1f23ddb03bd66b06e0616c50
Jonathan240105 <jonathan.quito@live.u-tad.com>
2026-05-30 23:58:54 +0200

    Commit añadido data class para la capa domain

--- app/src/main/java/com/example/snkrsapp/Domain/ModelPrincipal.kt
+++ app/src/main/java/com/example/snkrsapp/Domain/ModelPrincipal.kt
@@ -6,5 +6,19 @@ data class ModelPrincipal(
     val exitoMarcas: Boolean = false,
     val cargandoProductos: Boolean = false,
     val cargandoMarcas: Boolean = false,
-    val listaMarcas: List<Marca> = emptyList()
+    val listaMarcas: List<Marca> = emptyList(),
+
+    val esListaFiltrada: Boolean = false,
+    val esBusquedaTexto: Boolean = false,
+    val textoBusquedaActual: String = "",
+
+    val minPrecioSeleccionado: Double? = null,
+    val maxPrecioSeleccionado: Double? = null,
+    val tallaSeleccionado: Double? = null,
+    val marcasSeleccionadas: List<Int>? = null,
+
+    val minPrecio: Int? = null,
+    val maxPrecio: Int? = null,
+    val talla: Double? = null,
+    val marcas: List<Int>? = null,
 )
--- app/src/main/java/com/example/snkrsapp/Domain/Producto.kt
+++ app/src/main/java/com/example/snkrsapp/Domain/Producto.kt
@@ -5,7 +5,7 @@ data class Producto(
     val idMarca: Int = 0,
     val modelo: String = "",
     val precio: Int = 0,
-    val talla: Int = 0,
-    val uidVendedor: String = "",
-    val imagenUrl: String = ""
+    val talla: Int? = 0,
+    val uidVendedor: String? = "",
+    val imagenUrl: String? = ""
 )
